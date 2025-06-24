5b2de24760dde8ac35fc42930ff79fecad1d70d4
franckrm <franckrm2001@gmail.com>
2025-06-24 19:13:29 -0300

    feat: ensure user has clinic on doctors page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,14 +29,15 @@ export const auth = betterAuth({
                 }
             });
             //TODO: Ao adaptar para o usuário ter múltiplas clínicas, é necessário adaptar o código.
-            const clinic = clinics[0];
+            const clinic = clinics?.[0];
+
             return {
                 user: {
                     ...user,
-                    clinic: {
-                        id: clinic.clinicId,
-                        name: clinic.clinic.name,
-                    }
+                    clinic: clinic?.clinicId ? {
+                        id: clinic?.clinicId,
+                        name: clinic?.clinic.name,
+                    } : undefined
 
                 },
                 session
