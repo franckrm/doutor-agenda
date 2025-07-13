bd79b4f58a8232ba62de56db59e97e0c11395ffe
franckrm <franckrm2001@gmail.com>
2025-07-13 15:23:14 -0300

    feat: ensure disable days are shown on time select

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,10 +1,10 @@
-export const generateTimeSlot = () => {
+export const generateTimeSlots = () => {
     const slots = [];
-    for (let hours = 5; hours < 23; hours++) {
-        for (let minutes = 0; minutes < 60; minutes += 30) {
-            const timeString = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:00`;
+    for (let hour = 5; hour <= 23; hour++) {
+        for (let minute = 0; minute < 60; minute += 30) {
+            const timeString = `${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}:00`;
             slots.push(timeString);
         }
     }
     return slots;
-}
+};
